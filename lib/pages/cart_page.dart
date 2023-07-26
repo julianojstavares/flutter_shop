52c57013a271c55634c90feedd471f076b779db7
Juliano J. S. Tavares <julianojstavares@outlook.com>
2023-07-26 17:38:18 -0300

    Section 8, class 237

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,14 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/cart.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Carrinho"),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(25),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Total",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const SizedBox(width: 10),
+                  Chip(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    label: Text(
+                      "R\$ ${cart.totalAmount.toStringAsFixed(2).replaceAll(".", ",")}",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text("COMPRAR"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
