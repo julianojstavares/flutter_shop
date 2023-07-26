bfbe06c2cd30a96749228f9c7b3ebd39373d5101
Juliano J. S. Tavares <julianojstavares@outlook.com>
2023-07-26 19:10:21 -0300

    Section 8, class 241

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -14,6 +14,38 @@ class ProductDetailPage extends StatelessWidget {
         centerTitle: true,
         title: Text(product.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const SizedBox(height: 10),
+            Text(
+              "R\$ ${product.price.toStringAsFixed(2).replaceAll(".", ",")}",
+              style: const TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
